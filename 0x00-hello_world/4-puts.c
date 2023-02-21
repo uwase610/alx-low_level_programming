bfaf28f43f0faf03015c3606218dd3808aadbb13
Uwase <uwasegaellaolga@gmail.com>
2023-02-20 19:13:25 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that printsa given line using puts function
- * Returns - 0 for success
+ * main - A program that prints a line with puts
+ * Return: 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
