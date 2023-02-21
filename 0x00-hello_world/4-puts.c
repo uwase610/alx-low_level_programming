098598c7a3f0ae0ccdd784230fa130740335aa32
Uwase <uwasegaellaolga@gmail.com>
2023-02-20 18:44:34 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that printsa given line using puts function
+ * Returns - 0 for success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
