68e90deaf7715be345beccf3d8be2a0302a7a6f9
Uwase <uwasegaellaolga@gmail.com>
2023-02-23 14:15:21 -0800

    6. print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int dgt;
+
+	for (dgt = '0'; dgt <= '9'; dgt++)
+		putchar(dgt);
+	putchar('\n');
+	return (0);
+}
