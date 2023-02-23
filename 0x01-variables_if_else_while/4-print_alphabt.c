70b5a2be7224ca2ca0f9db2bd24ca7bdaa6e2258
Uwase <uwasegaellaolga@gmail.com>
2023-02-23 01:51:14 -0800

    alphabets except excluding e,q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program that prints alphabets except e and q
+ * Return: 0
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph != 'e' && alph != 'q')
+			putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
