0c6335f56a03a7a7928fd09ceaee7c7155864216
Uwase <uwasegaellaolga@gmail.com>
2023-03-01 15:12:32 -0800

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - print sign of a number
+ * @n: number sign to be checked
+ * Return: 1 if n is positive, 0 if n is zero, and -1 if negative number
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
