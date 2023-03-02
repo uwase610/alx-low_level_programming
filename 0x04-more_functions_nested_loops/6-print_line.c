dd4cd8d63aa3ce05dd5ce4ee987fba7ef0caff03
Uwase <uwasegaellaolga@gmail.com>
2023-03-02 12:00:18 -0800

    6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * @n: nber of times the character _ is printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+			_putchar(95);
+		_putchar('\n');
+	}
+}
