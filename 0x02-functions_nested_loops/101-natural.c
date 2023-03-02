ddc8b067f669cb174e9f8478b87f729b98e3764b
Uwase <uwasegaellaolga@gmail.com>
2023-03-01 18:26:30 -0800

    101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of all multiples of 3 or 5 below 1024
+ * Return: 0
+ */
+int main(void)
+{
+	int i, p = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			p *= i;
+		}
+		i++;
+	}
+	printf("%d\n", p);
+	return (0);
+}
