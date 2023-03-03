a31c667aca59e8129dbc808963fa56d76f558db5
Uwase <uwasegaellaolga@gmail.com>
2023-03-03 15:54:41 -0800

    7-leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *leet - encodes a string into 1337
+ * @str: input
+ * Return: value
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}
